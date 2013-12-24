f0bda98b9215c6d611f6d426797712a4efd2efb2
Willians Martins <willians.martins@digitalbox.cc>
2013-12-24 11:54:04 -0200

    cliente controller, listagem e inc alt de cliente

--- src/main/java/br/com/fiap/web/controller/ClienteController.java
+++ src/main/java/br/com/fiap/web/controller/ClienteController.java
@@ -15,52 +15,56 @@ public class ClienteController {
 	
 	private ClienteEntity entity;
 	private JpaGenericDao<ClienteEntity> dao = new ClienteDaoImpl();
-	
+	List<ClienteEntity> lista;
+
 	public ClienteController(){
 		entity = new ClienteEntity();
+		lista = dao.findAll();
 	}
 	
-	public String list(){
+	public String listagem(){
 		entity = new ClienteEntity();
+		lista = dao.findAll();
 		return "lista_clientes.xhtml?faces-redirect=true";
 	}
 	
-	public String save(){
+	public void save(){
 		dao.insert(entity);
 		entity = new ClienteEntity();
-		return "lista.xhtml";
+		listagem();
 	}
 	
-	public String remove(){
+	public void remove(){
 		dao.delete(entity.getId());
-		return "lista.xhtml";
+		listagem();
 	}	
 	
 	public String incAlt(){
 		entity = dao.findById(entity.getId());
-		return "inserir.xhtml";
+		return "inserir_cliente.xhtml";
 	}	
 	
 	public String prepareInsert(){
 		entity = new ClienteEntity();
 		System.out.println("insert");
-		return "inserir.xhtml?faces-redirect=true";
+		return "inserir_cliente.xhtml?faces-redirect=true";
 	}	
-	
-	public List<ClienteEntity> getListagem() {
-		return dao.findAll();
-	}
-
 	
 	//GETTERS AND SETTERS
 	
-	public ClienteEntity getCliente() {
+	public ClienteEntity getEntity() {
 		return entity;
 	}
 
-	public void setPedido(ClienteEntity entity) {
+	public void setEntity(ClienteEntity entity) {
 		this.entity = entity;
 	}
 	
-	
+	public List<ClienteEntity> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<ClienteEntity> lista) {
+		this.lista = lista;
+	}
 }
