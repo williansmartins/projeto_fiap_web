770320414381374709650ac087b25dd30f1c627b
Willians Martins <willians.martins@digitalbox.cc>
2014-03-03 09:46:46 -0300

    over nos botões do menu

--- web/src/main/webapp/js/scripts.js
+++ web/src/main/webapp/js/scripts.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
+	
 	$('.logo_clicavel2').on( "click", function() {
 		window.location = '/projeto_fiap_web'; 
 	});
+
+	$( '.menu li' ).hover( function() {
+		$elemento = $( this ).children();
+		$elemento.toggleClass( "active" );
+//		$( this ).('a').csscolor: rgb(255, 124, 16); */
+		/* 	text-decoration: none; */
+	});
+	
 } );
 
 function confirmacao(id){
